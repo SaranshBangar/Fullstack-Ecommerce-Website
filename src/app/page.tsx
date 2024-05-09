727a728ca283771048869d8a7edf5aad2c67786b
Saransh Bangar <saranshbangad@gmail.com>
2024-05-09 11:25:04 +0530

    Added framer motion, started review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -106,6 +107,7 @@ export default function Home() {
 
       <section className="bg-slate-100 py-24">
         <MaxWidthWrapper className="flex flex-col items-centre gap-16 sm:gap-32">
+          {/* User 1 review */}
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2">customers <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-8 text-green-500" /></span> say</h2>
             <img
@@ -123,9 +125,62 @@ export default function Home() {
                 <Star className="w-5 h-5 text-green-600 fill-green-600" />
                 <Star className="w-5 h-5 text-green-600 fill-green-600" />
               </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the design. Had the case for two and a half months now and <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had before the image started fading into yellow-ish colour after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* User 2 review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="w-5 h-5 text-green-600 fill-green-600" />
+                <Star className="w-5 h-5 text-green-600 fill-green-600" />
+                <Star className="w-5 h-5 text-green-600 fill-green-600" />
+                <Star className="w-5 h-5 text-green-600 fill-green-600" />
+                <Star className="w-5 h-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket and that lead to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>. I dig it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Josh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
---
+++ src/components/Reviews.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { useRef } from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+
+const PHONES = [
+    "/testimonials/1.jpg",
+    "/testimonials/2.jpg",
+    "/testimonials/3.jpg",
+    "/testimonials/4.jpg",
+    "/testimonials/5.jpg",
+    "/testimonials/6.jpg",
+]
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+    const result: Array<Array<T>> = []
+
+    for (let i = 0; i < array.length; i++) {
+        const index = i % numParts
+        if (!result[index]) {
+            result[index] = []
+        }
+        result[index].push(array[i]);
+    }
+
+    return result;
+}
+function ReviewGrid () {
+    const containerRef = useRef<HTMLDivElement | null>(null)
+
+    const isInView = useInView(containerRef, {once: true, amount: 0.4})
+
+    const columns = splitArray(PHONES, 3)
+    const column1 = columns[0]
+    const column2 = columns[1]
+    const column3 = splitArray(columns[2], 2)
+
+    return <div ref={containerRef} className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols 2 lg:grid-cols-3"></div>
+}
+
+export function Reviews () {
+    return (
+        <MaxWidthWrapper className="relative max-w-5xl">
+            <img
+                aria-hidden="true"
+                src="/what-people-are-buying.png"
+                className="absolute select-none hidden xl:block -left-32 top-1/3"
+            />
+
+            <ReviewGrid />
+        </MaxWidthWrapper>
+    )
+}
