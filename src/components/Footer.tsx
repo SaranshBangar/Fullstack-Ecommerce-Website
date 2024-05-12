f50b0559926bc5e45ddd02832efbaff1394ac11e
Saransh Bangar <saranshbangad@gmail.com>
2024-05-12 10:04:41 +0530

    Completed footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 
 const Footer = () => {
@@ -8,7 +9,27 @@ const Footer = () => {
                     className="border-t border-gray-200"
                 />
 
-                <div className="h-full flex flex-col md:flex-row md:justify-between"></div>
+                <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
+                    <div className="text-center md:text-left pb-2 md:pb-0">
+                        <p className="text-sm text-muted-foreground">
+                            &copy; {new Date().getFullYear()} All rights reserved
+                        </p>
+                    </div>
+
+                    <div className="flex items-center justify-center">
+                        <div className="flx space-x-8">
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                                Terms
+                            </Link>
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                                Privacy Policy
+                            </Link>
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                                Cookie Policy
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </MaxWidthWrapper>
         </footer>
     )
